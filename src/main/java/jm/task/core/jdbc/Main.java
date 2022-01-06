5d8827028e2cb3072caaa934e83fd787bccdec5d
AlkaK99 <alka3940@gmail.cim>
2022-01-06 17:56:35 +0900

    Доработанная

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userServ = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userServ.cleanUsersTable();
+
+        userServ.saveUser("Alex", "Li", (byte)22);
+        userServ.saveUser("Nina", "Lim", (byte)26);
+        userServ.saveUser("Stew", "Kim", (byte)67);
+        userServ.saveUser("Mike", "Li", (byte) 52);
+
+        userServ.removeUserById(1);
+        userServ.getAllUsers();
+        userServ.cleanUsersTable();
+        userServ.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost:3306/mynewtest?useSSL=false&serverTimezone=UTC";
+    private static final String user = "root";
+    private static final String password = "3940";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
